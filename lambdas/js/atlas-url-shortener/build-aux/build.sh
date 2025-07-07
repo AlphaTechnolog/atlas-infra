4751a83a142de081508ab485c02cb2342382cfb4
Gabriel Guerra <gfranklings@gmail.com>
2025-07-07 16:29:08 -0400

    fix: Adjust build process a lil bit

--- lambdas/js/atlas-url-shortener/build-aux/build.sh
+++ lambdas/js/atlas-url-shortener/build-aux/build.sh
@@ -1,9 +1,11 @@
 #!/usr/bin/env bash
 
+set -e
+
 cd "$(dirname "$0")"/../ || exit 1
 
 test -d dist && rm -rf dist
 npx tsc
-cd dist || exit 1
-rm -rf atlas-url-shortener-layer && mv atlas-url-shortener/src/* ./
-for x in atlas-url-shortener/{src,}; do rmdir "$x"; done
+cd dist/ || exit 1
+rm -rf layers
+mv lambdas/js/atlas-url-shortener/src/* ./ && rm -rf lambdas
